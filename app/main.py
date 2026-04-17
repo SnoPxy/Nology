08a0587f9e82ca75ee640eae17a04402f6faa728
Arthur Lopes <arthurlops04@gmail.com>
2026-04-17 16:17:17 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import datetime
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-from controller import consultaController
+from app.controller import consultaController
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
-from controller.cashbackController import CashbackController
-from controller.consultaController import ConsultaController
-from controller.tipoClienteController import TipoClienteController
+from app.controller.cashbackController import CashbackController
+from app.controller.consultaController import ConsultaController
+from app.controller.tipoClienteController import TipoClienteController
 
 
 app = FastAPI()
@@ -14,7 +14,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -25,18 +25,16 @@ controllerTipoCliente = TipoClienteController()
 
 
 @app.post("/calcular")
-
 async def calcular(request: Request):
     try:
         body = await request.json()
     except:
         body = await request.form()
 
-    tipo_id = int(body.get("tipo_id"))
-    valor = float(body.get("valor"))
-    
+    tipo_id = int(body.get("id_tipo_cliente"))
+    valor = float(body.get("valor_consulta"))
+
     resultado = controllerCashback.calculate_cashback(tipo_id, valor)
-   
 
     if not resultado:
         return JSONResponse(
@@ -51,31 +49,27 @@ async def calcular(request: Request):
         data_acesso=datetime.datetime.now(),
         tipo_id=tipo_id,
         id_cashback=resultado["id_cashback"],
-        valor=valor,   
+        valor=valor,
     )
 
     return JSONResponse(content=resultado)
 
 
-
 @app.get("/historico")
 def historico(request: Request):
     ip = request.client.host
-   
+
     dados = controllerConsulta.consult_ip(ip)
     lista = []
-    
-  
+
     for dado in dados:
-        
-        
+
         cashback = controllerCashback.get_cashback_by_id(dado["id_cashback"])
         tipo_cliente = controllerTipoCliente.get_id(dado["id_tipo_cliente"])
 
-        consulta = {"ip_usuario":dado['ip'],"data_acesso":dado['data_acesso'],"valor_consulta":dado['valor'],"id_tipo_cliente":tipo_cliente['id'], "nome_tipo_cliente" : tipo_cliente['nome'], "id_cashback":cashback["id"], "valor_cashback":cashback["valor"]}
+        consulta = {"ip_usuario": dado['ip'], "data_acesso": dado['data_acesso'], "valor_consulta": dado['valor'], "id_tipo_cliente": tipo_cliente['id'],
+                    "nome_tipo_cliente": tipo_cliente['nome'], "id_cashback": cashback["id"], "valor_cashback": cashback["valor"]}
 
         lista.append(consulta)
-        
+
     return JSONResponse(content=jsonable_encoder(lista))
-
-
