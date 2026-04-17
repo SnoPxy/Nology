5743b3949440a2e27b1b5fe4e6d2e1b2d80f8967
Arthur Lopes <arthurlops04@gmail.com>
2026-04-17 16:14:49 -0300

    Update consultaController.py

--- app/controller/consultaController.py
+++ app/controller/consultaController.py
@@ -1,13 +1,14 @@
-from model.modelConsulta import consultaModel
+from app.model.modelConsulta import consultaModel
 
 
 class ConsultaController:
-    
+
     def __init__(self):
         self.model = consultaModel()
 
     def save_consult(self, ip, data_acesso, tipo_id, id_cashback, valor):
-        self.model.insert_consult_BD(ip, data_acesso, tipo_id, id_cashback, valor)
+        self.model.insert_consult_BD(
+            ip, data_acesso, tipo_id, id_cashback, valor)
 
     def consult_ip(self, ip):
         return self.model.consult_ip(ip)
