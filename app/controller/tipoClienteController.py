b13fcf4075ba3a5839e89d460e04da192f98f097
Arthur Lopes <arthurlops04@gmail.com>
2026-04-17 16:15:20 -0300

    Update tipoClienteController.py

--- app/controller/tipoClienteController.py
+++ app/controller/tipoClienteController.py
@@ -1,19 +1,13 @@
-from model import modelTipoCliente
+from app.model import modelTipoCliente
+
 
 class TipoClienteController:
-    
+
     def __init__(self):
         self.model = modelTipoCliente.tipoClienteModel()
-        
+
     def get_id(self,  tipo_id):
-      return  self.model.get_id(tipo_id)
-  
+        return self.model.get_id(tipo_id)
+
     def closer_connection(self):
         self.model.close()
-  
-
-
-      
-    
-    
-    
